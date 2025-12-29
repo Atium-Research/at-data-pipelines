3a5157ee6ce3a16ed186146286128ea40aabe97b
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-12-28 19:26:48 -0700

    New headers logic.

--- pipelines/__main__.py
+++ pipelines/__main__.py
@@ -1,5 +1,4 @@
 import os
-import httpx
 from prefect import flow
 
 
@@ -9,4 +8,10 @@ def my_flow():
 
 
 if __name__ == "__main__":
+    cf_access_client_id = os.getenv("CF_ACCESS_CLIENT_ID")
+    cf_access_client_secret = os.getenv("CF_ACCESS_CLIENT_SECRET")
+
+    if cf_access_client_id and cf_access_client_secret:
+        os.environ["PREFECT_CLIENT_CUSTOM_HEADERS"] = f'{{"CF-Access-Client-Id": "{cf_access_client_id}", "CF-Access-Client-Secret": "{cf_access_client_secret}"}}'
+
     my_flow.serve(name="my-first-deployment", cron="* * * * *")
